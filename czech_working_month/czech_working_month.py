750eebf2824c8d93a04529eab336393213feffa9
Petr Belohlavek <me@petrbel.cz>
2017-10-19 18:31:05 +0200

    Add info of currently worked hours

--- czech_working_month/czech_working_month.py
+++ czech_working_month/czech_working_month.py
@@ -26,41 +26,50 @@ def _get_national_holiday(day: datetime.date) -> Optional[Holiday]:
     return None
 
 
-def show_working_month(month: int, year: int, hours_per_day: int, part_time_ratio: float) -> None:
+def show_working_month(day: int, month: int, year: int, hours_per_day: int, part_time_ratio: float) -> None:
     """Show overall month statistics."""
     next_month = month + 1 if month < 12 else 1
     next_year = year if month < 12 else year + 1
 
     total_working_days = 0
+    total_working_days_till_today = 0
     message = []
-    for day in _date_range(datetime.date(year, month, 1), datetime.date(next_year, next_month, 1)):
-        is_weekend = day.weekday() > 4
-        holiday = _get_national_holiday(day)
+    for i, _day in enumerate(_date_range(datetime.date(year, month, 1), datetime.date(next_year, next_month, 1))):
+        is_weekend = _day.weekday() > 4
+        holiday = _get_national_holiday(_day)
 
-        day_type = 'working day'
+        day_type = 'working _day'
         if holiday is not None:
             day_type = 'National holiday: {}'.format(holiday.name_en)
         elif is_weekend:
             day_type = 'weekend'
-        message.append([day, day_type])
+        message.append([_day, day_type])
 
         if is_weekend or holiday is not None:
             continue
 
         total_working_days += 1
+        if i < day:
+            total_working_days_till_today += 1
 
-    print(tabulate(message, headers=['date', 'day type']))
+    print(tabulate(message, headers=['date', '_day type']))
     print()
     print('----------------------')
     print('Total working days: {}'.format(total_working_days))
     print('Full-time working hours: {}'.format(total_working_days * hours_per_day))
+    print('Total working days (until today inclusive): {}'.format(total_working_days_till_today))
+    print('Full-time working hours: {}'.format(total_working_days_till_today * hours_per_day))
 
     if not math.isclose(part_time_ratio, 1.0, abs_tol=0.001):
         part_time_hours = int(total_working_days * hours_per_day * part_time_ratio)
+        part_time_hours_till_today = int(total_working_days_till_today * hours_per_day * part_time_ratio)
+
         print('{}-time working hours: {} (i.e. ~{:.2f} {}-hours days)'.format(part_time_ratio,
-                                                                         part_time_hours,
-                                                                         part_time_hours/hours_per_day,
-                                                                         hours_per_day))
+                                                                              part_time_hours,
+                                                                              part_time_hours/hours_per_day,
+                                                                              hours_per_day))
+        print('{}-time working hours: {} (i.e. ~{:.2f} {}-hours days) (until today inclusive)'.format(
+            part_time_ratio, part_time_hours_till_today, part_time_hours_till_today / hours_per_day, hours_per_day))
 
 
 def main():
@@ -75,10 +84,12 @@ def main():
 
     today = datetime.date.today()
 
+    current_day = today.day
     current_month = args.month or today.month
     current_year = args.year or today.year
 
-    show_working_month(month=current_month,
+    show_working_month(day=current_day,
+                       month=current_month,
                        year=current_year,
                        hours_per_day=args.day_length,
                        part_time_ratio=args.part_time_ratio)
